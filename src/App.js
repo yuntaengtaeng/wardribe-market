e3f0985b056bb2a9c414ab6560b56bc2c60bc4ad
yuntaengtaeng <k76253218@gmail.com>
2022-07-15 21:54:24 +0900

    add : styled reset module 추가
    - AppInner 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>;
-}
+import './index.css';
+import AppInner from './AppInner';
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <AppInner></AppInner>
+    </>
+  );
+};
 
 export default App;
---
+++ src/AppInner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppInner = () => {
+  return <div>AppInner</div>;
+};
+
+export default AppInner;
