b596b0ba896b22bf0b643dfccef32469a67f8703
yuntaengtaeng <k76253218@gmail.com>
2022-07-15 22:14:11 +0900

    feat : react-router를 사용하여 page 분리
    - Add : 게시글 추가
    - Chat : 채팅
    - Detail : 게시글 상세
    - Main : 메인
    - Profile : 프로필
    - Search : 검색 (사용하는 URL 2개 /search/:query => 검색 시, category/:category => 카테고리를 선택한 경우

--- src/AppInner.jsx
+++ src/AppInner.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import Header from './components/Ui/Header/Header';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Add from './pages/Add';
+import Detail from './pages/Detail';
+import Chat from './pages/Chat';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+
 const AppInner = () => {
-  return <div>AppInner</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        {['/search/:query', 'category/:category'].map((path) => (
+          <Route path={path} element={<Search />} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppInner;
